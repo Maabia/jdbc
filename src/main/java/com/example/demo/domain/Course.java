7c4c6001d17a9ef73cfe494b6183c8fb3815f831
Maabia <maabia.ferry16@gmail.com>
2022-07-18 14:48:18 -0300

    adicionando anotações

--- src/main/java/com/example/demo/domain/Course.java
+++ src/main/java/com/example/demo/domain/Course.java
@@ -3,16 +3,25 @@ package com.example.demo.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
 public class Course {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int courseId;
     private String title;
     private String description;
     private String link;
+    @javax.persistence.Id
+    private Long id;
 
     @Override
     public String toString() {
@@ -24,4 +33,11 @@ public class Course {
                 '}';
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
